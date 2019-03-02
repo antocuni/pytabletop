2d02c38a5e134223012c99ce340431851e5dc926
Antonio Cuni <anto.cuni@gmail.com>
2019-03-02 20:02:44 +0100

    proof of concept of different maps syncing

--- main.py
+++ main.py
@@ -109,14 +109,20 @@ class PyTableTopApp(App):
     def on_pause(self):
         return True
 
-    def do_reveal(self):
+    def do_sync(self):
         fog = self.root.ids.fog
-        newrect = RevealRectangle(pos=(50, 400), size=(250, 250))
-        fog.add_widget(newrect)
+        fog2 = self.root.ids.fog2
 
-    def do_clear(self):
-        pass
-        #self.root.ids.map.visible_areas[:] = []
+        # remove all the RevealRectangle from fog2
+        for w in fog2.children[:]:
+            if isinstance(w, RevealRectangle):
+                fog2.remove_widget(w)
+        
+        # readd new children
+        for rect in fog.children:
+            if isinstance(rect, RevealRectangle):
+                newrect = RevealRectangle(pos=rect.pos, size=rect.size)
+                fog2.add_widget(newrect)
 
 if __name__ == '__main__':
     PyTableTopApp().run()
