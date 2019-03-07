1398b26832c47ce711b18593877aace95159c629
Antonio Cuni <anto.cuni@gmail.com>
2019-03-07 19:12:05 +0100

    make it possible to specify the viewer address from the cmdline

--- dm/dm.py
+++ dm/dm.py
@@ -3,13 +3,17 @@
 import kivy
 kivy.require('1.0.6')
 
+import sys
 from kivy.app import App
+from kivy.properties import StringProperty
 from fogofwar import RevealRectangle
 
 
 class DMApp(App):
+    server = StringProperty('127.0.0.1')
 
-    REVEAL_URL = 'http://127.0.0.1:5000/reveal/'
+    def reveal_url(self):
+        return 'http://%s:5000/reveal/' % (self.server)
 
     def on_pause(self):
         return True
@@ -18,9 +22,12 @@ class DMApp(App):
         import requests
         fog = self.root.ids.fog
         areas = fog.get_json_areas()
-        resp = requests.post(self.REVEAL_URL, json=areas)
+        resp = requests.post(self.reveal_url(), json=areas)
         print resp
         print resp.text
 
 if __name__ == '__main__':
-    DMApp().run()
+    server = '127.0.0.1'
+    if len(sys.argv) >= 2:
+        server = sys.argv[1]
+    DMApp(server=server).run()
