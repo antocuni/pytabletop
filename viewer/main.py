19e34215850cd74b7842b58499c22dafb738f765
Antonio Cuni <anto.cuni@gmail.com>
2019-03-07 01:04:39 +0100

    rename viewer.py into main.py

--- viewer/main.py
+++ viewer/main.py
@@ -1,2 +1,25 @@
-from viewer import ViewerApp
-ViewerApp().run()
+#!/usr/bin/env python
+
+import kivy
+kivy.require('1.0.6')
+
+from kivy.app import App
+from server import ViewerServer
+from fogofwar import RevealRectangle
+
+
+class ViewerApp(App):
+
+    def __init__(self, *args, **kwargs):
+        super(ViewerApp, self).__init__(*args, **kwargs)
+        self.server = ViewerServer(kivy_app=self)
+        self.server.start()
+
+    def on_pause(self):
+        return True
+
+    def reveal(self, d):
+        self.root.ids.fog.set_json_areas(d)
+
+if __name__ == '__main__':
+    ViewerApp().run()
