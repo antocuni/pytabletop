3069fbe5f4a52bd68ee51b935b7c8502e2a93422
Antonio Cuni <anto.cuni@gmail.com>
2019-03-20 00:48:51 +0100

    use a ScreenManager for the viewer app, so that we can easily show popup content

--- viewer/main.py
+++ viewer/main.py
@@ -8,9 +8,15 @@ kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.properties import StringProperty
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
 from server import ViewerServer
 from fogofwar import RevealRectangle
+from manager import Manager
 from getip import getIP
+
+class MapScreen(Screen):
+    pass
 
 class ViewerApp(App):
 
@@ -22,11 +28,23 @@ class ViewerApp(App):
         self.server.start()
         self.IPAddress = getIP()
 
+    def build(self):
+        Window.bind(on_keyboard=self.on_keyboard)
+        self.mapscreen = MapScreen(name='map')
+        manager = Manager()
+        manager.open(self.mapscreen)
+        return manager
+
+    def on_keyboard(self, window, key, scancode, codepoint, modifier):
+        if key == 27: # ESC
+            return self.root.go_back()
+        return False
+
     def on_pause(self):
         return True
 
     def reveal(self, d):
-        self.root.ids.fog.set_json_areas(d)
+        self.mapscreen.ids.fog.set_json_areas(d)
 
 if __name__ == '__main__':
     ViewerApp().run()
---
+++ viewer/manager.py
@@ -0,0 +1,49 @@
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.behaviors.focus import FocusBehavior
+
+class Manager(ScreenManager):
+    
+    def __init__(self):
+        transition = SlideTransition(duration=.35)
+        super(Manager, self).__init__(transition=transition)
+        self.history = []
+
+    def open(self, view):
+        self.unfocus_maybe()
+        name = view.name
+        if self.has_screen(name):
+            self.remove_widget(self.get_screen(name))
+        self.add_widget(view)
+        self.transition.direction = 'left'
+        self.current = name
+        self.history.append(view)
+
+    def unwind(self, name):
+        while self.current_view.name != name:
+            res = self.go_back()
+            if not res:
+                # we reached the bottom without finding the target screen,
+                # bail out
+                break
+
+    def go_back(self):
+        if len(self.history) < 2:
+            return False
+        view = self.history.pop()
+        if hasattr(view, 'close'):
+            view.close()
+        self.transition.direction = 'right'
+        self.current = self.history[-1].name
+        return True
+
+    def unfocus_maybe(self):
+        if not self.history:
+            return
+        screen = self.history[-1]
+        for widget in screen.walk():
+            if isinstance(widget, FocusBehavior):
+                widget.focus = False
+
+    @property
+    def current_view(self):
+        return self.history[-1]
