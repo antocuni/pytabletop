8808759a3a90fa2ffdd67e4083d122f2648a97b6
Antonio Cuni <anto.cuni@gmail.com>
2019-06-30 16:05:20 +0200

    hopefully fix the deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,34 @@
 
 # usage: deploy.sh [--all]
 
-TMPDIR=/tmp/adbpush/pytt
-FILES=(
-    android.txt
-    main.py
-    pytt
-    )
-
-if [ x$1 = "x--all" ]
-then
-    FILES+=(libs)
-    adb shell rm -rf /sdcard/kivy/pytabletop
-fi
+TMPDIR=/tmp/adbpush/pytabletop
+FILES=(android.txt
+       main.py
+       libs
+       pytt
+      )
 
 rm -rf $TMPDIR
 mkdir -p $TMPDIR
-cp -L -r "${FILES[@]}" $TMPDIR
-cd $TMPDIR
-py.cleanup
-cd -
 
-adb push $TMPDIR /sdcard/kivy/pytabletop
+if [ x$1 = "x--all" ]
+then
+    # deploy everything -- slowish
+    adb shell rm -rf /sdcard/kivy/pytabletop
+    cp -L -r "${FILES[@]}" $TMPDIR
+    cd $TMPDIR
+    py.cleanup
+    cd -
+    adb push $TMPDIR /sdcard/kivy/pytabletop
+else
+    # deploy only pytt -- faster
+    cp -L -r pytt $TMPDIR
+    cd $TMPDIR
+    py.cleanup
+    cd -
+    adb shell rm -rf /sdcard/kivy/pytabletop/pytt
+    adb push $TMPDIR/pytt /sdcard/kivy/pytabletop
+fi
+
 adb logcat -c
 adb logcat -s python
